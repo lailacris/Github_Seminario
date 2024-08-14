d6263ed37a7126acd22afb96a16c3c9ede411f0b
Marcos Paulo Nogueira Mendes <mpnm10@hotmail.com>
2024-08-13 21:54:32 -0300

    first commit

---
+++ 1_Git/2-Comandos_Basicos.py
@@ -0,0 +1,28 @@
+'''
+    Alguns conceitos:
+        Branch:
+        Staging:
+        Commit:
+        Push:
+        Pull:
+
+    Alguns comandos básicos e importantes:
+        git init -> Cria um novo repositório Git em um diretório.
+        git add -> Adiciona um arquivo à área de staging.
+        git commit -m "Mensagem do commit": Cria um novo commit com a mensagem especificada.
+        git status -> Mostra o status atual do repositório, incluindo arquivos modificados, não rastreados e em staging.
+        git log -> Mostra o histórico de commits.
+        git branch -> Lista todos os branches locais.
+        git branch <nome_do_branch> -> Cria um novo branch.
+        git checkout <nome_do_branch> -> Muda para um branch existente.
+        git checkout -b <novo_branch> -> Cria um novo branch e muda para ele.
+        git branch -d <nome_do_branch> -> Remove um branch local.
+        git remote add origin <url> -> Adiciona um repositório remoto com o nome "origin".
+        git push origin main -> Envia as alterações locais para o branch "main" do repositório remoto.
+        git pull origin main -> Baixa as alterações do branch "main" do repositório remoto e as mescla no branch local.
+        git clone <url> -> Clona um repositório existente.
+        git diff: Mostra as diferenças entre os commits.
+        git reset HEAD^ -> Desfaz o último commit.
+        git revert <commit> -> Desfaz um commit específico.
+        
+'''
